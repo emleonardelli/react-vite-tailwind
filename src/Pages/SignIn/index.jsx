c59924d632c95d2a49b679dc593275b11e5b6b18
Ezequiel Mella <emleonardelli@gmail.com>
2023-06-17 11:15:39 -0300

    feat: navbar and layout

---
+++ src/Pages/SignIn/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../Components/Layout'
+
+function SignIn() {
+  return (
+    <Layout>
+      SignIn
+    </Layout>
+  )
+}
+
+export default SignIn
